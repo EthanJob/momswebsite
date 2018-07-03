3164dfa2470728b4b6368b9de18cc2ae77888ba1
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-07-03 14:41:41 -0400

    angular test functions work

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ const app = angular.module('MomsWebsite', []);
 
 app.controller('MainController', ['$http', function($http) {
 
-
+  this.test = () => {
+    console.log("it's connected");
+  }
 
 
 }]);
