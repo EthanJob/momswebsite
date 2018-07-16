bd30b0cce8bd0b273735196c14ad59d19a8fc125
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-07-16 13:35:12 -0400

    created test function for changing tabs

--- js/app.js
+++ js/app.js
@@ -4,7 +4,22 @@ app.controller('MainController', ['$http', function($http) {
 
   this.test = () => {
     console.log("it's connected");
-  }
+  };
+
+  this.home = true;
+  this.lunch = false;
+  this.dinner = false;
+  this.dessert = false;
+
+  this.changeTest = () => {
+    this.home = false;
+    this.dessert = true;
+    console.log("home =", this.home);
+    console.log("dessert =", this.dessert);
+  };
+
+  console.log("home =", this.home);
+  console.log("dessert =", this.dessert);
 
 
 }]);
