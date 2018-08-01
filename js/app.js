cd3cafcd16fabe61f7e1cd47db305fa971959952
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-07-31 20:00:18 -0400

    images link to their page

--- js/app.js
+++ js/app.js
@@ -24,16 +24,19 @@ app.controller('MainController', ['$http', function($scope, $http) {
   this.lunchFunc = () => {
     this.reset();
     this.lunch = true;
+    window.scrollTo(0,0);
   };
 
   this.dinnerFunc = () => {
     this.reset();
     this.dinner = true;
+    window.scrollTo(0,0);
   };
 
   this.dessertFunc = () => {
     this.reset();
     this.dessert = true;
+    window.scrollTo(0,0);
   };
   // functions^
 
